5c9d537cc876302a3a2a51051e184b91491eb8b5
alcantarajulio <julioalc21@gmail.com>
2024-03-10 12:45:44 -0300

    [add]: add fibonacci practice 2 solution

--- basic/fibonacci.hs
+++ basic/fibonacci.hs
@@ -1,14 +1,24 @@
 -- Júlio Alcantara - 122110504
 -- Paradigmas de Linguagem de Programação 2023.2 Prática 2
 
-fibonacci :: Integer -> Integer
+fibonacci :: Int -> Int
 fibonacci n
      | n == 0 = 0
      | n == 1 = 1
-     | otherwise = fibonacci (n-1) + fibonacci (n-2)
+     | otherwise = fibonacci (n - 1) + fibonacci (n - 2)
+
+checkFib :: Int -> Int -> Int
+checkFib sum n
+  | sumOfDigits (fibonacci n) == sum = fibonacci n
+  | otherwise = checkFib sum (n + 1)
+
+sumOfDigits :: Int -> Int
+sumOfDigits n
+  | n == 0 = 0
+  | otherwise = n `mod` 10 + sumOfDigits (n `div` 10)
+
 
 main :: IO()
 main = do
-    line <- getLine 
-    n <- readLn :: IO Integer
-    print(fibonacci n)
+    n <- readLn :: IO Int
+    print(checkFib n 0)
